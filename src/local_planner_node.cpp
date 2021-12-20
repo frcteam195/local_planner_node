f83636f03763878ada3c2b5eb13139bea247fdd9
Michael Todd <eltodd@gmail.com>
2021-12-20 05:24:56 +0000

    Switch to using ros helper interface

--- src/local_planner_node.cpp
+++ src/local_planner_node.cpp
@@ -5,16 +5,84 @@
 #include <string>
 #include <mutex>
 
-#include <teb_local_planner/FeedbackMsg.h>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <teb_local_planner/teb_local_planner_ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
 #pragma GCC diagnostic pop
-#include "local_planner.hpp"
 
-ros::NodeHandle* node;
+#include "local_planner_node/TrajectoryFollowCue.h"
 
 #define RATE (1)
+
+using namespace teb_local_planner;
+
+ros::NodeHandle* node;
+TebLocalPlannerROS * planner;
+
+tf2_ros::Buffer * tfBuffer;
+tf2_ros::TransformListener * tfListener;
+
+costmap_2d::Costmap2DROS * costmap;
+
+bool plan_active = false;
+
+void send_initial_plan()
+{
+    geometry_msgs::PoseStamped target;
+    tf2::Quaternion Up;
+    Up.setRPY(0,0,0);
+    target.pose.orientation.w = Up.getW();
+    target.pose.orientation.x = Up.getX();
+    target.pose.orientation.y = Up.getY();
+    target.pose.orientation.z = Up.getZ();
+    target.pose.position.x = 4;
+    target.pose.position.y = 0;
+    target.pose.position.z = 0;
+
+    target.header.stamp = ros::Time::now();
+    target.header.frame_id = "odom";
+
+    std::vector<geometry_msgs::PoseStamped> plan;
+    plan.push_back(target);
+    planner->setPlan(plan);
+    plan_active = true;
+}
+
+void planner_loop(void)
+{
+    while(ros::ok())
+    {
+        local_planner_node::TrajectoryFollowCue active_trajectory;
+        geometry_msgs::Twist result;
+        if(plan_active)
+        {
+            planner->computeVelocityCommands(result);
+            if(planner->isGoalReached())
+            {
+                ROS_INFO("Goal Reached");
+                plan_active = false;
+            }
+            active_trajectory.velocity.linear.x = result.linear.x;
+            active_trajectory.velocity.linear.y = result.linear.y;
+            active_trajectory.velocity.linear.z = result.linear.z;
+
+            active_trajectory.velocity.angular.x = result.angular.x;
+            active_trajectory.velocity.angular.y = result.angular.y;
+            active_trajectory.velocity.angular.z = result.angular.z;
+
+            active_trajectory.traj_follow_active = true;
+        }
+        else
+        {
+            active_trajectory.traj_follow_active = false;
+        }
+
+        static ros::Publisher active_traj_publisher = node->advertise<local_planner_node::TrajectoryFollowCue>("/active_trajectory", 1);
+        active_traj_publisher.publish(active_trajectory);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -22,42 +90,18 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle n;
 	node = &n;
-    ros::Rate rate(RATE);
 
-    ros::Publisher output_pub =
-        node->advertise<teb_local_planner::FeedbackMsg>("/teb_feedback", 1);
+    tfBuffer = new tf2_ros::Buffer();
+    planner = new TebLocalPlannerROS();
+    tfListener = new tf2_ros::TransformListener(*tfBuffer);
+    costmap = new costmap_2d::Costmap2DROS("costmap", *tfBuffer);
 
-    LocalPlanner planner(node);
+    planner->initialize("", tfBuffer, costmap);
+    send_initial_plan();
 
-    while( ros::ok() )
-    {
+    std::thread planner_thread(planner_loop);
 
-        planner.step();
-
-        /*
-        teb_local_planner::FeedbackMsg output_traj;
-
-        teb_local_planner::TrajectoryMsg traj;
-        teb_local_planner::TrajectoryPointMsg traj_point;
-
-        traj_point.velocity.linear.x = 10;
-        traj_point.time_from_start = ros::Duration(1.0);
-        traj.trajectory.push_back( traj_point );
-
-        traj_point.velocity.linear.x = 10;
-        traj_point.velocity.linear.y = 10;
-        traj_point.time_from_start = ros::Duration(2.0);
-        traj.trajectory.push_back( traj_point );
-
-        output_traj.trajectories.push_back( traj );
-        output_pub.publish(output_traj);
-        */
-
-
-        ros::spinOnce();
-        rate.sleep();
-    }
-
+    ros::spin();
 
 	return 0;
 }
